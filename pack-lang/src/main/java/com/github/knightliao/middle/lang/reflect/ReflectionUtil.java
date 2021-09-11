a2348ebdf0d343b7a7f4b65785e51a623ea3afbf
knightliao <knightliao@gmail.com>
2021-09-12 01:59:49 +0800

    +reflection

---
+++ pack-lang/src/main/java/com/github/knightliao/middle/lang/reflect/ReflectionUtil.java
@@ -0,0 +1,27 @@
+package com.github.knightliao.middle.lang.reflect;
+
+import java.lang.reflect.Constructor;
+
+import com.github.knightliao.middle.lang.exceptions.exceptions.others.SystemException;
+
+/**
+ * @author knightliao
+ * @email knightliao@gmail.com
+ * @date 2021/9/12 01:57
+ */
+public class ReflectionUtil {
+
+    public static <T> T newWithConstructor(String className) {
+
+        try {
+
+            Class<T> metaClass = (Class<T>) Class.forName(className);
+            Constructor<T> con = metaClass.getConstructor();
+            return con.newInstance();
+
+        } catch (Exception ex) {
+
+            throw new SystemException("new instance failed " + className, ex);
+        }
+    }
+}
