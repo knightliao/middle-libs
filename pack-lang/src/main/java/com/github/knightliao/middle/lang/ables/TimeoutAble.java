accdb74ebe6bfc2698967023b6b14094bbfd5da6
knightliao <knightliao@gmail.com>
2021-09-11 16:38:53 +0800

    re

---
+++ pack-lang/src/main/java/com/github/knightliao/middle/lang/ables/TimeoutAble.java
@@ -0,0 +1,11 @@
+package com.github.knightliao.middle.lang.ables;
+
+/**
+ * @author knightliao
+ * @email knightliao@gmail.com
+ * @date 2021/9/11 16:35
+ */
+public interface TimeoutAble {
+
+    int getTimeoutMs();
+}
